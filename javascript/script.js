38c34530778677dca36a98bce5bf038b034a4109
WhitelightningDev <danielmommsen@hotmail.com>
2023-08-06 01:03:57 +0200

    removed 3 html pages and recreated the home page

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,6 @@
 
-
-// Smooth scroll to the bottom of the screen when the anchor button is clicked
-document.querySelector('.intro-contact').addEventListener('click', function (event) {
-    event.preventDefault();
-    const footer = document.querySelector('footer'); // Replace 'footer' with the ID or class of the element you want to scroll to
-    footer.scrollIntoView({ behavior: 'smooth' });
-});
-
-
-
-
 // Function to open Gmail
+
 function openGmail() {
     // Replace "mailto:youremail@gmail.com" with your Gmail address
     const gmailAddress = "https://mail.google.com/";
